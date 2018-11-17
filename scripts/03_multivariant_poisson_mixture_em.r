2d5f679775ed8060b8c0667139559c4f091cd99c
Sergio Garcia Prado <sergio@garciparedes.me>
2018-11-17 14:21:38 +0100

    Done with multivariant poisson mixtures

--- scripts/03_multivariant_poisson_mixture_em.r
+++ scripts/03_multivariant_poisson_mixture_em.r
@@ -13,23 +13,92 @@ k <- 3
 # Cases
 n <- 1000
 
-#
-p.x <- c(0.3, 0.4)
-lambda.x <- c(1, 5, 10)
-x1 <- rpois(n * p.x[1], lambda.x[1])
-x2 <- rpois(n * p.x[2], lambda.x[2])
-x3 <- rpois(n * (1 - sum(p.x)), lambda.x[3])
+p <- matrix(c(0.1, 0.5,
+              0.6, 0.2,
+              0.3, 0.3),
+            3, 2, byrow = TRUE)
 
-p.y <- c(0.1, 0.6)
-lambda.y <- c(10, 50, 70)
+lambda <- matrix(c( 1, 10,
+                    5, 50,
+                   10, 70),
+                 3, 2, byrow = TRUE)
 
-y1 <- rpois(n * p.y[1], lambda.y[1])
-y2 <- rpois(n * p.y[2], lambda.y[2])
-y3 <- rpois(n * (1 - sum(p.y)), lambda.y[3])
+x <- sapply(1:d, function(i) {
+    sapply(1:k, function(j){
+      rpois(n * p[j, i], lambda[j, i])
+    }) %>%
+    unlist()
+  }) %>%
+  unlist() %>%
+  matrix(n, d)
 
 
+LogLikeliHoodPoisson <- function(lambda, x) {
+ logL <- sapply(1:length(lambda), function(i) {
+      sum(dpois(x[, i], lambda[i], log = TRUE))
+    }) %>%
+    sum()
+ return(logL)
+}
 
-data <- matrix(c(x1, x2, x3, y1, y2, y3),
-               n, d)
+NegativeLogLikeliHoodPoisson <- function(...) {
+ - LogLikeliHoodPoisson(...)
+}
 
-data
+OptimizeLambda <- function(x) {
+  # opt <- optim(runif(2), NegativeLogLikeliHoodPoisson, x = x,
+  #              lower = 10e-4, upper = Inf, method = "L-BFGS-B")
+  # lambda.hat <- opt$par
+  lambda.hat <- colMeans(x)
+  return(lambda.hat)
+}
+
+CalculateB <- function(x, lambda) {
+  apply(x, 1, function(obs) {
+    sapply(1:nrow(lambda), function(i) {
+         sum(dpois(obs, lambda[i, ], log = TRUE))
+      }) %>% which.max()
+  })
+}
+
+
+OptimizeMultiVariantPoissonMixtureEM <- function(x, k, d) {
+  lambda <- (1:d) %>%
+    sapply(function(i) {
+      sort(runif(k, min = min(x[, i]), max = max(x[, i])))
+    })
+
+  b <- CalculateB(x, lambda)
+  b.old <- rep(0, n)
+
+  i <- 0
+  while (any(b != b.old)) {
+    # Fijamos criterio de convergencia en la estabilización de etiquetas de la mixtura.
+    # Ya que cuando esta se estabiliza, los EMV de lambda no variarán.
+
+    i <- i + 1
+    lambda <- sapply(1:k, function(j) {
+      OptimizeLambda(x[b == j, ])
+    }) %>% t()
+
+    b.old <- b
+    b <- CalculateB(x, lambda)
+  }
+
+  p <- (1:k) %>%
+   sapply(function(i) {
+     mean(b == i)
+   })
+
+  result <- list(lambda = lambda,
+                 group = b,
+                 steps = i,
+                 ratios = p
+               )
+  return(result)
+}
+
+a <- OptimizeMultiVariantPoissonMixtureEM(x, k, d)
+
+
+plot(x, col = a$group + 1)
